8905e67ff2623523aeb6aac83ffde2cd1aa4b880
yiyaoyi <916262480@qq.com>
2015-05-11 20:28:10 +0800

    Create Circle.h

---
+++ Shape/Circle.h
@@ -0,0 +1,16 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+#include "Shape.h"
+class Circle:public Shape
+{
+    public:
+        void outself()
+        {
+            printf("Im a circle.\n");
+        }
+    protected:
+    private:
+};
+
+#endif // CIRCLE_H
