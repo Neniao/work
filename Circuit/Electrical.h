6f0cc0868628230825606d0ef180b0fe9e929d90
yiyaoyi <916262480@qq.com>
2015-05-17 11:04:44 +0800

    Create Electrical.h

---
+++ Circuit/Electrical.h
@@ -0,0 +1,15 @@
+#ifndef ELECTRICAL_H
+#define ELECTRICAL_H
+
+
+class Electrical
+{
+    public:
+        virtual void work()
+        {
+        }
+    protected:
+    private:
+};
+
+#endif // ELECTRICAL_H
